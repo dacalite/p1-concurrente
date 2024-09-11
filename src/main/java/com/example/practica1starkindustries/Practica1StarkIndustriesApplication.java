ca986a8c1adf818e746b7959579132dbbaa6d877
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-09-11 10:50:23 +0200

    initial commit

---
+++ src/main/java/com/example/practica1starkindustries/Practica1StarkIndustriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica1starkindustries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1StarkIndustriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica1StarkIndustriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practica1starkindustries/Practica1StarkIndustriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica1starkindustries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1StarkIndustriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
